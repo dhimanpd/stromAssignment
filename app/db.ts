ab1d6eb31c3841dadb97d6aa364ed6f1637c9e81
Prince <dhimanpd@users.noreply.github.com>
2024-05-02 02:58:34 +0200

    Update db.ts

--- app/db.ts
+++ app/db.ts
@@ -25,7 +25,7 @@ export async function getUser(email: string, password:string) {
     // redirect: "follow" 
   };
   
-  return await fetch("https://c523-165-22-64-171.ngrok-free.app/api/auth/jwt/login", requestOptions)
+  return await fetch(`${process.env.NGURL}api/auth/jwt/login`, requestOptions)
   .then((response) => response.json())
     .then((result) => {return result})
     .catch((error) => {return null;});
